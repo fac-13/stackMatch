fb51c65837da63edec49560881151e47cc1e5b0c
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-08 17:06:31 +0100

    set up db build and connection with testing relates #10

---
+++ src/model/database/db_connection.js
@@ -0,0 +1,26 @@
+const pgp = require('pg-promise')();
+const url = require('url');
+require('dotenv').config();
+
+let DB_URL = process.env.DB_URL;
+
+if (process.env.NODE_ENV === 'test') {
+  DB_URL = process.env.TEST_DB_URL;
+}
+
+if (!DB_URL) throw new Error('Environment variable DB_URL must be set');
+
+const params = url.parse(DB_URL);
+const [username, password] = params.auth.split(':');
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== 'localhost',
+};
+
+module.exports = pgp(options);
