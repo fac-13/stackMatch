e56613ba4078aa1895352377c53635a8159f836c
Helen Zhou <helen.zhou6@gmail.com>
2018-05-09 15:32:57 +0100

    GitHub OAuth work in progress Relate #26

--- src/index.js
+++ src/index.js
@@ -3,19 +3,15 @@ const https = require('https');
 const fs = require('fs');
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const httpsOptions = {
   key: fs.readFileSync('./keys/key.pem'),
   cert: fs.readFileSync('./keys/cert.pem')
 };
 
-app.listen(port, () => {
-  console.log(`app is running on http://${host}:${port}`);
-});
-
 const server = https
   .createServer(httpsOptions, app)
   .listen(port, () => {
-    console.log(`app is running on http://${host}:${port}`);
+    console.log(`app is running on https://${host}:${port}`);
   });
