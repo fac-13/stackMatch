98845127231247ceb3cf55b5c98ae18b076ea1dc
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-10 11:38:29 +0100

    add db master changes

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const { QueryFile } = require('pg-promise');
-const db = const path = require('path');
-const { QueryFile } = require('pg-promise');
 const db = require('./db_connection');
 
 const sql = file => QueryFile(path.join(__dirname, file), { minify: true });
