03f574010745580d971ae79b2e6aa7daaa25ade7
Helen Zhou <helen.zhou6@gmail.com>
2018-05-10 17:11:06 +0100

    Add middleware to protected routes and create my profile details edit route Closes #35

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,14 +2,25 @@ const router = require('express').Router();
 const passport = require('passport');
 
 const home = require('./home');
+const profileDetails = require('./profileDetails');
 // const auth = require('./auth');
 
 router.get('/', home.get);
 
+// MIDDLEWARE to ensure used is authenticated
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    return next();
+  }
+  // denied - should redirect to login
+  res.redirect('/');
+}
+
 // auth routes
 router.get(
   '/auth/github/signup',
-  passport.authenticate('github', { scope: ['read:org'] }),
+  passport.authenticate('github', { scope: ['read:org'] })
 );
 
 router.get(
@@ -30,6 +41,12 @@ router.get('/auth/github/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
+
+router.get(
+  '/myprofile/mydetails/edit', ensureAuthenticated, profileDetails.get
+);
+
+
 // router.get('/auth/github/signup', auth.signup);
 // router.get('/auth/github/callback', auth.signupCallback);
 // router.get('/auth/github/logout', auth.logout);
