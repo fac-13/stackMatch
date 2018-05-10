fd562cb572ca22c1921b895033c8bd03cae8f2c6
tdoran <tomgdoran@gmail.com>
2018-05-10 14:07:56 +0100

    write postMemberInfo query Closes #56

--- src/model/queries/postMemberInfo.js
+++ src/model/queries/postMemberInfo.js
@@ -1,7 +1,14 @@
-const db = ('../database/db_connection');
+const db = require('../database/db_connection');
 
 const postMemberInfo = (githubJson) => {
-  // const { github_ }
+  const values = Object.values(githubJson);
+  return db.query(
+    'INSERT INTO members (github_id, github_handle, first_name, last_name, github_avatar_url) VALUES ($1, $2, $3, $4, $5)',
+    values,
+  ).then(() => console.log(`${githubJson.github_handle}member info posted to DB`))
+    .catch((error) => {
+      throw new Error(error.message);
+    });
 };
 
 module.exports = postMemberInfo;
