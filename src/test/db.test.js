5af17c1f1101417938b9d7c8b3c1d732833e4e24
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-05-10 12:53:42 +0100

    add test to check member data relates #43

--- src/test/db.test.js
+++ src/test/db.test.js
@@ -49,6 +49,39 @@ test('Test get member data and is the correct format', (t) => {
     });
 });
 
+test('Test member data to ensure correct data received', (t) => {
+  runDbBuild()
+    .then(() => getMemberData(1))
+    .then((res) => {
+      const correctResult = {
+        id: 1,
+        github_id: 1,
+        first_name: 'Helen',
+        last_name: 'Zhou',
+        github_handle: 'helenzhou6',
+        github_avatar_url: 'https://uk.linkedin.com/dbsmith',
+        fac_campus: 'london',
+        fac_code_id: 1,
+        linkedin_url: 'https://uk.linkedin.com/',
+        twitter_handle: 'hel_zhou',
+        member_type: 'admin',
+        job_search_status: 'red',
+        min_years_exp: 0,
+        max_years_exp: 1,
+        github_cv_url: 'https://github.com/helenzhou6/CV',
+        cv_url: 'https://github.com/helenzhou6/CV',
+        job_view_pref: 'private',
+      };
+      t.equal(Object.keys(res).length, 17, 'correct array length');
+      t.deepEqual(res, correctResult, 'deepEquals of first test member');
+      t.end();
+    })
+    .catch((error) => {
+      console.log(error.message);
+      t.error(error, 'build error');
+    });
+});
+
 test.onFinish(() => {
   dbConnection.$pool.end();
 });
