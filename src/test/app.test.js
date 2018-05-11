b5f8e359640abfe54b2ab0a67c8ef5b83e17f46a
Helen Zhou <helen.zhou6@gmail.com>
2018-05-11 12:19:26 +0100

    Merge conflict resolution

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -9,15 +9,15 @@ test('Test if tape is working', (t) => {
 
 test(`Test if Express app is running on http://${process.env.HOST}:${
   process.env.PORT
-  } or http://localhost:3000/`, (t) => {
-    request(app)
-      .get('/')
-      .end((err, res) => {
-        t.ok(res, `response received with status code: ${res.status}`);
-        t.error(err, 'no server error');
-        t.end();
-      });
-  });
+} or http://localhost:3000/`, (t) => {
+  request(app)
+    .get('/')
+    .end((err, res) => {
+      t.ok(res, `response received with status code: ${res.status}`);
+      t.error(err, 'no server error');
+      t.end();
+    });
+});
 
 test('Test if home route gets status code 200 and is html', (t) => {
   request(app)
@@ -45,6 +45,17 @@ test('Test if /auth/github/signup route redirects to Github', (t) => {
 test('Test if /auth/github/callback route redirects', (t) => {
   request(app)
     .get('/auth/github/callback')
+    .expect(302)
+    .end((err, res) => {
+      t.equal(res.statusCode, 302, 'Should return 302');
+      t.error(err, 'no server error');
+      t.end();
+    });
+});
+
+test('Test if /auth/github/logout route redirects', (t) => {
+  request(app)
+    .get('/auth/github/logout')
     .expect(302)
     .end((err, res) => {
       t.equal(res.statusCode, 302, 'Should return 302');
