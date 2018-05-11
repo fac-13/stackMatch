b5f297b43b6fb1b53273e75dd22fd57d18adb3a3
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-11 13:14:04 +0100

    add error handling

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,4 +1,5 @@
 const test = require('tape');
+const sinon = require('sinon');
 const request = require('supertest');
 const app = require('./../app.js');
 
@@ -25,18 +26,19 @@ test('Test if home route gets status code 200 and is html', (t) => {
     .expect(200)
     .expect('Content-Type', /html/)
     .end((err, res) => {
-      t.equal(res.statusCode, 200, 'Should return 200');
+      t.equal(res.statusCode, 200, 'should return 200');
       t.error(err, 'no server error');
       t.end();
     });
 });
 
+// AUTHENTICATION TESTS
 test('Test if /auth/github/signup route redirects to Github', (t) => {
   request(app)
     .get('/auth/github/signup')
     .expect(302)
     .end((err, res) => {
-      t.equal(res.statusCode, 302, 'Should return 302');
+      t.equal(res.statusCode, 302, 'should return 302');
       t.error(err, 'no server error');
       t.end();
     });
@@ -47,7 +49,7 @@ test('Test if /auth/github/callback route redirects', (t) => {
     .get('/auth/github/callback')
     .expect(302)
     .end((err, res) => {
-      t.equal(res.statusCode, 302, 'Should return 302');
+      t.equal(res.statusCode, 302, 'should return 302');
       t.error(err, 'no server error');
       t.end();
     });
@@ -58,8 +60,36 @@ test('Test if /auth/github/logout route redirects', (t) => {
     .get('/auth/github/logout')
     .expect(302)
     .end((err, res) => {
-      t.equal(res.statusCode, 302, 'Should return 302');
+      t.equal(res.statusCode, 302, 'should return 302');
       t.error(err, 'no server error');
+      t.end();
+    });
+});
+
+// ERROR HANDLING ROUTES
+test('Test if server returns 404 on invalid route', (t) => {
+  request(app)
+    .get('/notavalidroute')
+    .expect(404)
+    .end((err, res) => {
+      t.equal(res.statusCode, 404, 'should return 404');
+      t.error(err, 'no server error');
+      t.end();
+    });
+});
+
+
+// TODO 500 server error test
+test('Test if server returns 500 on server error', (t) => {
+  request(app)
+    .get('/brokenroute')
+    .end((err, res) => {
+      t.ok(
+        res.text.includes('Internal Server Error'),
+        'response includes "internal server error" message for client',
+      );
+      t.equal(res.statusCode, 500, 'should return 500');
+      t.error(err, 'server error');
       t.end();
     });
 });
