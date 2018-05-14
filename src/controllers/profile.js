b45fd103a3a752026f973d070ed25709b29522cb
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-05-14 16:40:53 +0100

    respond to code review relates #96

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -3,13 +3,10 @@ const { saveProfileData } = require('../model/queries/');
 
 exports.get = (req, res) => {
   const user = addUserStatus(req);
-  console.log(user);
   res.render('profile', { activePage: { profile: true }, user });
 };
 
 exports.postDetails = (req, res) => {
-  // post user data (req.body) to database
-  console.log('form data: ', req.body);
   saveProfileData(req.body, req.user.github_id)
     .then(() => res.redirect(`/myprofile/${req.user.github_id}`))
     .catch(err => console.log(err));
