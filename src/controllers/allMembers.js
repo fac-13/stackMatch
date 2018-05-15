5ea945f867682e2e863116384b3a6f3175ab3330
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-15 12:58:07 +0100

    fix inner join to left join in getAllMemberData query

--- src/controllers/allMembers.js
+++ src/controllers/allMembers.js
@@ -1,28 +1,6 @@
 const { addUserStatus } = require('./middleware');
 const { getAllMemberData } = require('../model/queries/');
 
-// // placeholder until index.js is merged from PR request
-// const { ...allMembers } = [{
-//   id: 1,
-//   github_id: 1,
-//   full_name: 'Helen',
-//   github_handle: 'helenzhou6',
-//   github_avatar_url: 'https://uk.linkedin.com/dbsmith',
-//   fac_cohort: 'FAC0',
-//   tech_stack: ['JavaScript', 'Node.js'],
-//   job_search_status: 'red',
-// },
-// {
-//   id: 2,
-//   github_id: 2,
-//   full_name: 'Deborah',
-//   github_handle: 'dsmith',
-//   github_avatar_url: 'https://uk.linkedin.com/dbsmith',
-//   fac_cohort: 'FAC1',
-//   tech_stack: ['Node.js', 'JavaScript'],
-//   job_search_status: 'orange',
-// }];
-
 exports.get = (req, res) => {
   const user = addUserStatus(req);
   getAllMemberData()
--- src/model/queries/getAllMemberData.js
+++ src/model/queries/getAllMemberData.js
@@ -4,21 +4,18 @@ const allMembersQuery =
 `SELECT mem.id, mem.github_id, mem.full_name, mem.github_handle, github_avatar_url, code.code AS fac_cohort, 
 (SELECT array_agg(tech.tech ORDER BY stack.order_num) 
      FROM tech_stack tech
-     INNER JOIN member_tech_stack stack
+     LEFT JOIN member_tech_stack stack
      ON stack.stack_id = tech.id
      WHERE stack.member_id = mem.id
 ) AS tech_stack, job_search_status 
 FROM members mem
-INNER JOIN fac_code code
+LEFT JOIN fac_code code
 ON mem.fac_code_id = code.id`;
 
 const getAllMemberData = () =>
   dbConnection
     .query(allMembersQuery)
-    .then((allMemberDetails) => {
-      console.log('query result:', allMemberDetails);
-      return allMemberDetails;
-    })
+    .then(allMemberDetails => allMemberDetails)
     .catch(err => console.log(err));
 
 module.exports = getAllMemberData;
