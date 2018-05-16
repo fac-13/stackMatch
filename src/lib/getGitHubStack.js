a9ab29cd97766cf8535a7bdce86b7d6dc20c3b30
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-16 11:37:50 +0100

    set up

---
+++ src/lib/getGitHubStack.js
@@ -0,0 +1,17 @@
+
+const getGitHubRepos = () => {
+  // argument passed in - accessToken and github_id
+  // return an array of objects, apil calls for Repos
+};
+
+
+const getGitHubStack = () => {
+  // argument passed in - accessToken and github_id
+  // return an array of languages used in github Repos
+};
+
+module.exports = {
+  getGitHubRepos,
+  getGitHubStack,
+};
+
---
+++ src/test/api.test.js
@@ -0,0 +1,18 @@
+const test = require('tape');
+
+const getGitHubStack = require('../lib/getGitHubStack');
+
+
+test('GITHUB API TESTS', (t) => {
+  t.ok(true, 'tape is working');
+  t.end();
+});
+
+
+// GITHUB API UNIT TESTS
+test('Test getGitHubStack', (t) => {
+  // getGitHubStack()
+  // argument passed in - accessToken and github_id
+  // return an array of languages used in github Repos
+  t.end();
+});
