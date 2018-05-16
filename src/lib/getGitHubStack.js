0dd9552dd74976ffc3e24399d2a295b685408eeb
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-16 14:24:31 +0100

    updated comment

--- src/lib/getGitHubStack.js
+++ src/lib/getGitHubStack.js
@@ -1,7 +1,7 @@
 const request = require('axios');
 
 // argument passed in - accessToken and github_id
-// return an array of objects, apil calls for Repos
+// return an array of strings of the languages used in user's repos
 const getGitHubRepoLanguages = (accessToken, github_handle) => request.get(`https://api.github.com/users/${github_handle}/repos?type=all&sort=updated&direction=desc&access_token=${accessToken}`)
   .then((result) => {
     const repos = result.data;
