c1934cc3761376ec1a01bc6ecd8f4afda68d3b2b
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-16 12:41:48 +0100

    broken tests

--- src/lib/getGitHubStack.js
+++ src/lib/getGitHubStack.js
@@ -1,13 +1,23 @@
+const request = require('axios');
 
-const getGitHubRepos = () => {
-  // argument passed in - accessToken and github_id
-  // return an array of objects, apil calls for Repos
+
+const getGitHubRepos = (accessToken, github_handle) => {
+  console.log(github_handle);
+  console.log(accessToken);
+  return request.get(`https://api.github.com/users/${github_handle}/repos?type=all&sort=updated&direction=desc&access_token=${accessToken}`)
+    .then((repos) => {
+      const result = JSON.parse(JSON.stringify(repos));
+      console.log(result);
+      return result;
+    });
 };
+// argument passed in - accessToken and github_id
+// return an array of objects, apil calls for Repos
 
 
-const getGitHubStack = () => {
-  // argument passed in - accessToken and github_id
-  // return an array of languages used in github Repos
+const getGitHubStack = (accessToken, github_handle, repos) => {
+  // argument passed in - accessToken and array with repo language urls
+  // return an array of string languages used in github Repos
 };
 
 module.exports = {
--- src/test/api.test.js
+++ src/test/api.test.js
@@ -1,6 +1,7 @@
 const test = require('tape');
-
-const getGitHubStack = require('../lib/getGitHubStack');
+// const request = require('supertest');
+const { getGitHubRepos, getGitHubStack } = require('../lib/getGitHubStack');
+require('dotenv').config();
 
 
 test('GITHUB API TESTS', (t) => {
@@ -10,9 +11,13 @@ test('GITHUB API TESTS', (t) => {
 
 
 // GITHUB API UNIT TESTS
-test('Test getGitHubStack', (t) => {
-  // getGitHubStack()
+test('Test getGitHubRepos', (t) => {
+  getGitHubRepos(process.env.APIKEY, 'vlbee').then((actual) => {
+    console.log(typeof actual);
+    t.ok(Array.isArray(actual), 'returns an array');
+    t.end();
+  }).catch(err => console.log(err.message));
+
   // argument passed in - accessToken and github_id
   // return an array of languages used in github Repos
-  t.end();
 });
