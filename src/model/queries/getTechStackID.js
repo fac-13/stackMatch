102fa858ec97e51fd76e8397e47da89a2f50fcad
Helen Zhou <helen.zhou6@gmail.com>
2018-05-16 14:08:25 +0100

    Add 'LOWER()' to SQL query to compensate different cases

--- src/model/queries/getTechStackID.js
+++ src/model/queries/getTechStackID.js
@@ -2,7 +2,7 @@ const db = require('../database/db_connection');
 
 const getTechStackID = (techName) => {
   return db.query(
-    'SELECT id FROM tech_stack WHERE tech = $1',
+    'SELECT id FROM tech_stack WHERE LOWER(tech) = LOWER($1)',
     [techName],
   ).then(res => res[0]);
 };
