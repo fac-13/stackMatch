e0faeb23f6e567689817e6dcc913f640bd2e50d6
vlbee <32312712+vlbee@users.noreply.github.com>
2018-05-16 23:09:12 +0100

    add notmember-view

---
+++ src/model/queries/postNewMemberTechStack.js
@@ -0,0 +1,24 @@
+const {
+  addMemberTechStack,
+  getTechStackID,
+  getAllTechStack,
+  addTechStack,
+  deleteMemberTech,
+  getMemberTechStack,
+} = require('./query_tech-stack');
+
+
+const postNewMemberTechStack = (memberRepoStack) => {
+  getAllTechStack().then((allTech1) => {
+    const techNames = [];
+    allTech1.forEach(tech => techNames.push(tech.tech));
+    memberRepoStack.forEach(tech => (techNames.includes(tech) ? null : addTechStack(tech)));
+    getAllTechStack().then((allTech2) => {
+      console.lo;
+    });
+  });
+};
+
+postNewMemberTechStack();
+
+module.exports = postNewMemberTechStack;
