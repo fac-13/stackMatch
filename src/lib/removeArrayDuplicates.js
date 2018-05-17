97b6ec6d1ff99abf8411d6d72b6c849f758e4b61
Helen Zhou <helen.zhou6@gmail.com>
2018-05-17 11:16:33 +0100

    Refactor code to reuse lowerCaseArray helper function as per code review

--- src/lib/removeArrayDuplicates.js
+++ src/lib/removeArrayDuplicates.js
@@ -1,8 +1,9 @@
+const lowerCaseArray = require('./lowerCaseArray');
+
 const removeArrayDuplicates = (arrayInput) => {
   return arrayInput.reduce((acc, curr) => {
-    const lowerCaseAcc = acc.map(function (item) {
-      return item.toLowerCase();
-    });
+    const lowerCaseAcc = lowerCaseArray(acc);
+
     if (!lowerCaseAcc.includes(curr.toLowerCase())) {
       acc.push(curr);
     }
